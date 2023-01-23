233b22189873ed2c466398af16cad2cdb1f6ace2
sogabrie <gabrielyan.sos@mail.ru>
2023-01-23 15:00:19 +0400

    END libft 23.01.23 15:00

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: sogabrie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 15:24:19 by sogabrie          #+#    #+#             */
-/*   Updated: 2023/01/22 23:11:20 by sogabrie         ###   ########.fr       */
+/*   Updated: 2023/01/23 14:58:15 by sogabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 	int		j;
 
-	if (!s1 || !s2 ||
-		!(str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
+	if (!s1 || !s2)
+		return (0);
+	str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
 		return (0);
 	i = 0;
 	while (s1[i] != '\0')
